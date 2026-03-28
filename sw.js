164478dd5f8e249bcb68fe13ae5ee0fcafd17ffc
dudom10 <dudom10@gmail.com>
2026-03-27 21:58:00 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,10 +2,11 @@ const CACHE_NAME = 'fi-forecast-v1';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  'https://unpkg.com/react@18/umd/react.production.min.js',
-  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
-  'https://unpkg.com/recharts@2.12.7/umd/Recharts.js',
-  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdn.jsdelivr.net/npm/react@18.2.0/umd/react.production.min.js',
+  'https://cdn.jsdelivr.net/npm/react-dom@18.2.0/umd/react-dom.production.min.js',
+  'https://cdn.jsdelivr.net/npm/prop-types@15.8.1/prop-types.min.js',
+  'https://cdn.jsdelivr.net/npm/recharts@2.5.0/umd/Recharts.js',
+  'https://cdn.jsdelivr.net/npm/@babel/standalone@7.23.9/babel.min.js',
   'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600;700&family=IBM+Plex+Mono:wght@400;500;600&display=swap'
 ];
 
